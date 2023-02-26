48b85198ebfca2be8355d72bd04572a2f4c015d7
NazarPrysiazhnyi <2008Nazar@gmail.com>
2023-02-26 23:16:17 +0200

    Initial Commit

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by Nazar Prysiazhnyi on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
